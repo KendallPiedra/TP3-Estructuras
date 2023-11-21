b7408f94ea1df4dd339f222d5e6ea8fd745a7fe9
KendallPiedra <114965407+KendallPiedra@users.noreply.github.com>
2023-11-21 00:46:58 -0600

    dsf

---
+++ SKYNET/src/skynet/GraphApplet.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package skynet;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+/**
+ *
+ * @author Usuario
+ */
+
+public class GraphApplet extends Applet {
+    public void paint(Graphics g) {
+        // Aquí puedes agregar código para dibujar stu grafo utilizando
+        // las funciones de la clase Graphics.
+        g.drawString("¡Hola, Applet!", 20, 20);
+    }
+}

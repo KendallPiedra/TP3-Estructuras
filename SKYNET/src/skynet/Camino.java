2876988e984bd8b15eb5046b6356217981abb3b8
Kristhel Cordero <129348691+KristhelCordero@users.noreply.github.com>
2023-11-23 19:56:32 -0600

    Update Camino.java

--- SKYNET/src/skynet/Camino.java
+++ SKYNET/src/skynet/Camino.java
@@ -9,4 +9,11 @@ public class Camino {
     String ciudad1, ciudad2;
     int ejercito, bienes, distancia;
     
+    Camino(String ciudad1, String ciudad2, int ejercito, int bienes, int distancia){
+        this.ciudad1=ciudad1;
+        this.ciudad2=ciudad2;
+        this.ejercito=ejercito;
+        this.bienes=bienes;
+        this.distancia=distancia;
+    }
 }

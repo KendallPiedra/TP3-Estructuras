67c8bb5ecd4d81b9be6c22c66e4a86f7f777396f
Kristhel Cordero <129348691+KristhelCordero@users.noreply.github.com>
2023-11-26 21:00:53 -0600

    asasd

--- SKYNET/src/skynet/Grafo.java
+++ SKYNET/src/skynet/Grafo.java
@@ -165,27 +165,6 @@ public class Grafo {
     }
     
     public void eliminarCamino(Camino camino){
-        boolean borrado=false;
-        NodoGrafo temp=primero;
-        while(temp!=null){
-            Camino caminoTemp=temp.lista.primero;
-            while(caminoTemp!=null){
-                if(camino.origen.nombre.equals(caminoTemp.origen.nombre)&&camino.destino.nombre.equals(caminoTemp.destino.nombre)){
-                    temp.lista.eliminarAdyacencia(camino.destino);
-                    borrado=true;
-                    break;
-                }
-                caminoTemp= caminoTemp.siguiente;
-            }
-            if(borrado){
-                break;
-            }
-            temp=temp.siguiente;
-        }
-    }
-    
-    
-    public void eliminarCamino2(Camino camino){
        NodoGrafo temp=primero;
        while(temp.dato!=camino.origen){
            temp=temp.siguiente;

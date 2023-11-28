4d9638e80dbe0af982ac5458baef044a06a41267
Kristhel Cordero <129348691+KristhelCordero@users.noreply.github.com>
2023-11-28 12:33:40 -0600

    asdad

--- SKYNET/src/skynet/fgfff.java
+++ SKYNET/src/skynet/fgfff.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package skynet;
 
 /**
@@ -25,8 +22,11 @@ public class fgfff {
         System.out.println("===============LINEA DIVISORA===============");
         System.out.println(SKINEY.mapaSimulacion.grafo.toString());
         System.out.println("====================================LINEA DIVISORA GRAAAAANDE===================================");
-        System.out.println(SKINEY.generarRedDeUnSoloRecorrido());
-        //SKINEY.eliminarNodosMasVisitadosRecorrido();
+        System.out.println(SKINEY.generarRedDeUnSoloRecorridoSinAniquilacion());
+        SKINEY.eliminarNodosMasVisitadosRecorrido();
+        System.out.println("===============LINEA DIVISORA===============");
+        System.out.println(SKINEY.mapaSimulacion.grafo.toString());
+        SKINEY.eliminarNodosMasVisitadosRecorrido();
         System.out.println("===============LINEA DIVISORA===============");
         System.out.println(SKINEY.mapaSimulacion.grafo.toString());
         
